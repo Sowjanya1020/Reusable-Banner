1f0cdcf14789fbc0f1b195c0e00c78d05e30aedd
sowjanya <sowjanya1020@outlook.com>
2024-01-11 20:33:55 +0530

    frist commit

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+const Banneritem = () => (
+    <div>
+    <div className = "main-container" >
+    <h1 className = "first">The Season Latest</h1>
+    <p className = "p-1">Get the seasons all the latest in a flick of your hands </p>
+    <botton className = "button-1">Show More</botton>
+    </div>
+    <div className = "center-container" >
+    <h1 className = "first">Our New Designs</h1>
+    <p className = "p-1">Get the all designs for the our team by yourself</p>
+    <botton className = "button-1">Show More</botton>
+    </div>
+    <div className = "last-container" >
+    <h1 className = "first">Insiders</h1>
+    <p className = "p-1">Get the top class products for your self with extra offers </p>
+    <botton className = "button-1">Show More</botton>
+    </div>
+    </div>
+)
+
+export default Banneritem
